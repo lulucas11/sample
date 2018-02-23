9b7173a1459a470d5e9c6a2441276668b4352ff9
lucas <lulucas11@foxmail.com>
2018-02-23 06:04:32 +0000

    Finish user signup

--- resources/views/layouts/default.blade.php
+++ resources/views/layouts/default.blade.php
@@ -3,15 +3,16 @@
   <head>
     <title>@yield('title', 'Sample') - 旺旺旺LUCAS</title>
     <link rel="stylesheet" href="/css/app.css">
-      </head>
-      <body>
-        @include('layouts._header')
+</head>
+ <body>
+   @include('layouts._header')
 
-        <div class="container">
-          <div class="col-md-offset-1 col-md-10">
-            @yield('content')
-            @include('layouts._footer')
-          </div>
-        </div>
-      </body>
-    </html>
+   <div class="container">
+     <div class="col-md-offset-1 col-md-10">
+       @include('shared._messages')
+       @yield('content')
+       @include('layouts._footer')
+     </div>
+   </div>
+ </body>
+</html>

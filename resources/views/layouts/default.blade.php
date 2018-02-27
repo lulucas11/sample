66f4fba0c4c2da9e9740dd204d23fe49ad86abef
lucas <lulucas11@foxmail.com>
2018-02-27 14:12:41 +0000

    Finish login and logout

--- resources/views/layouts/default.blade.php
+++ resources/views/layouts/default.blade.php
@@ -1,18 +1,20 @@
 <!DOCTYPE html>
 <html>
   <head>
-    <title>@yield('title', 'Sample') - 旺旺旺LUCAS</title>
+    <title>@yield('title', 'Sample App') - lucas 旺旺旺</title>
     <link rel="stylesheet" href="/css/app.css">
-</head>
- <body>
-   @include('layouts._header')
+  </head>
+  <body>
+    @include('layouts._header')
 
-   <div class="container">
-     <div class="col-md-offset-1 col-md-10">
-       @include('shared._messages')
-       @yield('content')
-       @include('layouts._footer')
-     </div>
-   </div>
- </body>
+    <div class="container">
+      <div class="col-md-offset-1 col-md-10">
+        @include('shared._messages')
+        @yield('content')
+        @include('layouts._footer')
+      </div>
+    </div>
+
+    <script src="/js/app.js"></script>
+  </body>
 </html>

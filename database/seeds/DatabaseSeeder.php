7c8879b37f46bf78bdc777be1a146b7e371f6485
lucas <lulucas11@foxmail.com>
2018-03-12 08:33:08 +0000

    Add account activations

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -1,16 +1,21 @@
 <?php
 
 use Illuminate\Database\Seeder;
+use Illuminate\Database\Eloquent\Model;
 
 class DatabaseSeeder extends Seeder
 {
     /**
      * Run the database seeds.
      *
-     * @return void
+     // * @return void
      */
     public function run()
     {
-        // $this->call(UsersTableSeeder::class);
+        Model::unguard();
+
+        $this->call(UsersTableSeeder::class);
+
+        Model::reguard();
     }
 }
